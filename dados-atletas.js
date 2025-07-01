c45e213fe079a3c8889bcb2beebe06afde6bec7c
marcelomsdev <81388067+marcelomsdev@users.noreply.github.com>
2025-06-30 22:04:56 -0300

    Create dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,10 @@
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+console.log("Nome:", atleta.obtemNomeAtleta());
+console.log("Idade:", atleta.obtemIdadeAtleta());
+console.log("Peso:", atleta.obtemPesoAtleta());
+console.log("Altura:", atleta.altura);
+console.log("Notas:", atleta.obtemNotasAtleta());
+console.log("Categoria:", atleta.obtemCategoria());
+console.log("IMC:", atleta.obtemIMC());
+console.log("Média válida:", atleta.obtemMediaValida());
